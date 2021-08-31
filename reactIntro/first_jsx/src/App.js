4348379ed0f3da71bc7cbefa998f4c9d0abdc9a0
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2021-08-31 13:44:14 -0700

    first react lecture

---
+++ reactIntro/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React!</h1>
+      <p>Reasons to learn React</p>
+      <ul>
+        <li>It is the most popular front-end framework</li>
+        <li>It can be integrated with any api and backend</li>
+        <li>It is fun!</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
