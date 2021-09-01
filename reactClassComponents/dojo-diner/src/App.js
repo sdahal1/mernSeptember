bc77ce06c89028055735681d79139e36c3aaf19f
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2021-09-01 13:27:29 -0700

    state explained

--- reactClassComponents/dojo-diner/src/App.js
+++ reactClassComponents/dojo-diner/src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <h1>Dojo Diner! Take a look at our tasty menu!</h1>
       <p>Thank you for supporting small businesses!</p>
-      <Menu nameOfDish= "Momo" price = "$12.99">
+      <Menu nameOfDish= "Momo" price = "$12.99" numMealsServed = {10}>
           <p>A Nepali dish basically like a dumpling</p>
           <i>Vegan gluten free options available!</i>
       </Menu>
-      <Menu nameOfDish= "Maduros" price = "$15.99">
+      <Menu nameOfDish= "Maduros" price = "$15.99" numMealsServed = {5}>
           <p>Sweet Plantains straight from the Chilean Mountains!</p>
       </Menu>
-      <Menu nameOfDish= "Buffalo Chicken Pizza" price = "$20.99">
+      <Menu nameOfDish= "Buffalo Chicken Pizza" price = "$20.99" numMealsServed = {12}>
           <p>Straight out of the Buffalo NY location!</p>
           <i>Contains Wheat**</i>
       </Menu>
